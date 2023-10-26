1cb8def8e7021e7514c4a30f13f27b26b3e46c28
= <=>
2023-10-26 03:22:00 +0200

    working code phase1

--- stockprice_client/client.py
+++ stockprice_client/client.py
@@ -1,18 +1,19 @@
 import rpyc
 import time
 import logging
-import random 
+import random
+
 class StockPriceClient:
     def __init__(self, host, port):
         self.conn = rpyc.connect(host, port)
-        self.total_response_time  = 0
+        self.total_response_time = 0
         self.num_responses = 0
 
-    def fetch_stock_prices(self,symbols):
+    def fetch_stock_prices(self, symbols):
         try:
             # Time before the RPC call
             start_time = time.perf_counter_ns()
-            current_prices = self.conn.root.exposed_get_current_price(symbols)
+            current_prices = self.conn.root.exposed_fetch_stock_data({"client_id": "client1", "symbols": symbols})
             # After receiving the response
             end_time = time.perf_counter_ns()
             response_time = end_time - start_time
@@ -26,15 +27,21 @@ class StockPriceClient:
         except Exception as e:
             logging.error(f'Error fetching stock data: {str(e)}')
             return None
-        
+
 if __name__ == "__main__":
-    client = StockPriceClient("server1", 8000)
+    logging.basicConfig(level=logging.INFO)
+    client = StockPriceClient("server1", 1111)
     # List of symbols to fetch prices for
-    symbols = ["TSLA","GOOG","MSFT",'AMZN', 'NVDA']
+    symbols = ["AAPL", "TSLA", "GOOG", "MSFT", "AMZN", "NVDA"]
     prices = client.fetch_stock_prices(symbols)
-    #fetch data from the server 
-    for item in prices:
-        print(f"Symbol: {item} and Price: $ {prices[item]} ")
+
+    if prices:
+        for item in prices:
+            symbol = item['symbol']
+            price = item['data']['close']
+            print(f"Symbol: {symbol} and Price: ${price}")
+    else:
+        print("Error fetching stock prices.")
     
     # Sleep for a random amount of time between 0.5 seconds and 1 seconds
     sleep_time = random.uniform(0.5, 1)
